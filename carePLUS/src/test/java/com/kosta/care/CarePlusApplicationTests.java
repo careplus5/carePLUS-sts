6f5b993e9728f32f0655c2926057c33e28794e7f
샘 <108646100+HHansaem@users.noreply.github.com>
2024-05-29 16:48:23 +0900

    create carePLUS project

---
+++ carePLUS/src/test/java/com/kosta/care/CarePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
