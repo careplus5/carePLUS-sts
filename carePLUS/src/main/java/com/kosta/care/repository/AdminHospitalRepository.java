fdc24e73aa48b33f64d6d7c9a9344e57ac72aa27
yebrung <officeguider@naver.com>
2024-06-14 10:14:05 +0900

    wnrdj

--- carePLUS/src/main/java/com/kosta/care/repository/AdminHospitalRepository.java
+++ carePLUS/src/main/java/com/kosta/care/repository/AdminHospitalRepository.java
@@ -1,11 +1,9 @@
 package com.kosta.care.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-=======
->>>>>>> branch 'main' of https://github.com/careplus5/carePLUS-sts.git
+//github.com/careplus5/carePLUS-sts.git
 import org.springframework.stereotype.Repository;
 
 import com.kosta.care.entity.AdminHospital;
@@ -17,7 +15,4 @@ public interface AdminHospitalRepository extends JpaRepository<AdminHospital,Lon
 	@Modifying
 	@Query("UPDATE Admission a set a.admissionDischargeDate = :admissionDischargeDate, a.admissionDischargeOpinion =:admissionDischargeOpinion,  a.admissionStatus =:admissionStatus WHERE a.admissionNum = :admissionNum ")
 	public void findByAdmissionNum(Long admissionNum);
-	
-
-	AdminHospital findByAdmNum(Long admNum);
 }
